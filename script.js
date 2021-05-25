1302fb444f9eea1eb2a29a3410a526f6dad4193c
jlcareglio <jlcareglioalbornoz@gmail.com>
2021-05-25 19:03:11 -0300

    Background anim eliminado y simplificaciones aplicadas

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 var coll = document.getElementsByClassName("collapsible");
-var background = document.getElementsByClassName("background");
-var i;
 
-for (i = 0; i < coll.length; i++) {
+for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
-      background[0].style.height =
-        background[0].offsetHeight - content.scrollHeight + "px";
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-      background[0].style.height =
-        background[0].offsetHeight + content.scrollHeight + "px";
     }
   });
 }
